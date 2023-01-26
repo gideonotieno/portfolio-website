b6228c8960fefa52d2d38495d2a83f31c3af01ad
gideon <120317684+gideonotieno@users.noreply.github.com>
2023-01-26 16:27:57 +0300

    Update Type.js

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -7,9 +7,9 @@ function Type() {
       options={{
         strings: [
           "Developer",
-          "Deep Learning Engineer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "Machine Learning Engineer",
+          "Full Stack Developer",
+          "React Developer",
         ],
         autoStart: true,
         loop: true,
