4ef8ef12d81715cae82f23bea22c8f79c4e95b92
gideon <120317684+gideonotieno@users.noreply.github.com>
2023-01-26 16:17:44 +0300

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -21,8 +21,8 @@ function Home() {
               </h1>
 
               <h1 className="heading-name">
-                I'M
-                <strong className="main-name"> GIDEON OTIENO</strong>
+                I'm
+                <strong className="main-name"> Gideon Otieno</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
