e9a54e8164e4cd7612e5cde99a0cd87e26ece2ec
gideon <120317684+gideonotieno@users.noreply.github.com>
2023-01-26 21:20:13 +0300

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -104,7 +104,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/gideonotieno/portfolio-website"
                 target="_blank"
                 className="fork-btn-inner"
               >
