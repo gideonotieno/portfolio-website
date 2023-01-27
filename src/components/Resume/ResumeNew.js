d1e6253ca3621ba318f45d8b70e30fd6f652dd42
gideon <120317684+gideonotieno@users.noreply.github.com>
2023-01-27 09:55:01 +0300

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/gideonotieno/portfolio-website/src/components/myCV.pdf";
+  "https://gideonotieno/portfolio-website/src/components/myCV.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
