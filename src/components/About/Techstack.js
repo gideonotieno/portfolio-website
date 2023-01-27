02e05ef4271ffc2b843056fa228cb91d31fdada1
gideon <120317684+gideonotieno@users.noreply.github.com>
2023-01-27 12:04:32 +0300

    Update Techstack.js

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -7,10 +7,8 @@ import {
   DiNodejs,
   DiMongodb,
   DiPython,
-  DiGit,
 } from "react-icons/di";
 import {
-  SiPytorch,
   SiFirebase,
   SiNextdotjs,
 } from "react-icons/si";
@@ -34,19 +32,10 @@ function Techstack() {
         <DiMongodb />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiNextdotjs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
         <SiFirebase />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiPython />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPytorch />
       </Col>
     </Row>
   );
