817df91cc39cf6d0c44edd5fedfd60c9f885636a
gideon <120317684+gideonotieno@users.noreply.github.com>
2023-01-28 15:13:31 +0300

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,7 +5,7 @@ import Particle from "../Particle";
 import leaf from "../../Assets/Projects/leaf.png";
 import emotion from "../../Assets/Projects/emotion.png";
 import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
+import brain-seg from "../../Assets/Projects/brain-seg.png";
 import bitsOfCode from "../../Assets/Projects/blog.png";
 
 function Projects() {
