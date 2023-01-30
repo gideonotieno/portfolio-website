43fbac89a81590333a775d40723495702f8a217c
gideon <120317684+gideonotieno@users.noreply.github.com>
2023-01-30 09:49:04 +0300

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,7 +4,6 @@ import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import leaf from "../../Assets/Projects/leaf.png";
 import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
 
 import bitsOfCode from "../../Assets/Projects/blog.png";
 
