f6e898b81bd6932aadadb86e3ce9c55b0d892404
gideon <120317684+gideonotieno@users.noreply.github.com>
2023-02-12 20:43:39 +0300

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -21,7 +21,7 @@ function About() {
               paddingBottom: "50px",
             }}
           >
-            <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
+            <h1 style={{ fontSize: "2.0em", paddingBottom: "20px" }}>
               Know Who <strong className="purple">I'M</strong>
             </h1>
             <Aboutcard />
